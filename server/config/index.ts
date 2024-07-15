dc73c2c90733f7001352b294a5424ec4bbbd51aa
Mayur Patil <mayurpatil5680@gmail.com>
2024-07-15 12:48:46 +0530

    first commit

--- server/config/index.ts
+++ server/config/index.ts
@@ -48,20 +48,6 @@ export const logFile = {
   filePath: "/home/creat/Logs/",
 };
 
-export const strip = {
-  publishablekey:
-    "pk_live_51JhVWqLH6n6raFZu8UPbRQGT2Ba2jRQ3J4QFgHjZJQa5IxPbPcDYcMh4SpIGWr6AIlWVeljyuElLvMECZQv0NlZw00bptuQQHO",
-  secretkey:
-    "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",
-};
 
-export const zadama = {
-  userKey: "cbf82dd312f2d839405f",
-  secretKey: "8423efe92378b9f3b5a1",
-};
 
-export const twilioCredentials = {
-  accountSid: "ACf2127ebe0d30dd0892b6dbcd49481994",
-  authToken: "7fc8dfffd048a82a71a7da11630ae92a",
-  messagingServiceSid: "MGd7873bce2a7fbc82fc4b06b11fea4234",
-};
+
